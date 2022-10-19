fbe46bc00d1896a0bc2eba08442cdf933c389e95
vaibhav c <83589185+Vaibhav1934@users.noreply.github.com>
2022-10-19 08:40:21 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -114,7 +114,8 @@ USE_TZ = True
 STATIC_HOST = os.environ.get("DJANGO_STATIC_HOST", "")
 STATIC_URL = STATIC_HOST + "/static/"
 #STATIC_URL = "/static/"
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'static'),
+                  '/static/assets/vendor/swiper/']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
