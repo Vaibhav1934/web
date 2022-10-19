2c70fd8e6a8f1f0a0d031e6ea15307db45c7f179
vaibhav c <83589185+Vaibhav1934@users.noreply.github.com>
2022-10-19 08:32:02 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -111,13 +111,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-#STATIC_URL = "/static/"
-#STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+STATIC_URL = "/static/"
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 #CRISPY_TEMPLATE_PACK = 'bootstrap4'
-STATIC_HOST = os.environ.get("DJANGO_STATIC_HOST", "")
-STATIC_URL = STATIC_HOST + "/static/"
+#STATIC_HOST = os.environ.get("DJANGO_STATIC_HOST", "")
+#STATIC_URL = STATIC_HOST + "/static/"
