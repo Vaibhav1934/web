c0f666640b9ae5b29521eb34d5f4a595d75ce035
vaibhav c <83589185+Vaibhav1934@users.noreply.github.com>
2022-10-19 08:30:00 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -68,7 +68,7 @@ TEMPLATES = [
         },
     },
 ]
-STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+#STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 WSGI_APPLICATION = "myproject.wsgi.application"
 
 
@@ -111,8 +111,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "/static/"
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+#STATIC_URL = "/static/"
+#STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
